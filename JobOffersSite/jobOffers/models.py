59fbb25bff20507afb86eb63677eb220f5ee8190
Robert Wójtowicz <robivw508@gmail.com>
2021-11-11 17:16:29 +0100

    created first models

---
+++ JobOffersSite/jobOffers/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    creation_date = models.DateTimeField('creation date')
+    mail = models.CharField(max_length=50)
+    official_site = models.CharField(max_length=100)
+    city = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+
+
+class JobOffer(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    pub_date = models.DateTimeField('date published')
+    finish_date = models.DateTimeField('date offer ends')
+
+
+
+    
